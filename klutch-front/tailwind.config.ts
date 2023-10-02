f2f1f70ff35a07a5fa8b5387903d3649b5b15460
Fecef <fecef.figueiredo@gmail.com>
2023-10-02 16:53:23 -0300

    chore(setup): add app images, css-variables, metadata and favicon

---
+++ klutch-front/tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary1: "#228a95",
+        primary2: "#187680",
+        secondary1: "#ef9c4b",
+        secondary2: "#fd8030",
+        font: "#777777",
+        whiteFixed: "#fff",
+        backGround: "#f8f8f8",
+      },
+      fontSize: {
+        "1": "2.2rem",
+        "2": "2.5rem",
+        "3": "2.9rem",
+        "4": "3rem",
+        "5": "3.2rem",
+        "6": "4rem",
+        "7": "5.6rem",
+        "8": "6.3rem",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
